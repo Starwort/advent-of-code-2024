928c30f8b6add5f472f7c5466b029d0b86dedf84
starwort <tcphone93@gmail.com>
2024-12-01 05:02:46 +0000

    50/51

--- day_01.py
+++ day_01.py
@@ -25,7 +25,7 @@ raw = aoc_helper.fetch(1, 2024)
 
 
 def parse_raw(raw: str):
-    return ...
+    return extract_ints(raw)
 
 
 data = parse_raw(raw)
@@ -35,20 +35,30 @@ data = parse_raw(raw)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    ...
+    return (
+        data.chunked(2)
+        .mapped(list)
+        .transposition()
+        .mapped(sorted)
+        .mapped(list)
+        .transposition()
+        .mapped(lambda i: abs(i[0] - i[1]))
+        .sum()
+    )
 
 
-aoc_helper.lazy_test(day=1, year=2024, parse=parse_raw, solution=part_one)
+# aoc_helper.lazy_test(day=1, year=2024, parse=parse_raw, solution=part_one)
 
 
 # providing this default is somewhat of a hack - there isn't any other way to
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    ...
+    a, b = data.chunked(2).mapped(list).transposition()
+    return a.mapped(lambda i: i * b.count(i)).sum()
 
 
-aoc_helper.lazy_test(day=1, year=2024, parse=parse_raw, solution=part_two)
+# aoc_helper.lazy_test(day=1, year=2024, parse=parse_raw, solution=part_two)
 
 aoc_helper.lazy_submit(day=1, year=2024, solution=part_one, data=data)
 aoc_helper.lazy_submit(day=1, year=2024, solution=part_two, data=data)
