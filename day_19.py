3b90711154d53e58479bcecc54faf538654598e4
starwort <tcphone93@gmail.com>
2024-12-19 05:08:44 +0000

    332/464

--- day_19.py
+++ day_19.py
@@ -1,4 +1,5 @@
 from collections import Counter, defaultdict, deque
+from functools import cache
 
 import aoc_helper
 from aoc_helper import (
@@ -25,17 +26,32 @@ raw = aoc_helper.fetch(19, 2024)
 
 
 def parse_raw(raw: str):
-    return ...
+    a, b = raw.split("\n\n")
+    return list(a.split(", ")), list(b.split("\n"))
 
 
 data = parse_raw(raw)
+
+
+@cache
+def make(s: str, a: list[str]):
+    if not s:
+        return 1
+    tot = 0
+    for x in a:
+        if s.startswith(x):
+            tot += make(s[len(x) :], a)
+    return tot
 
 
 # providing this default is somewhat of a hack - there isn't any other way to
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    ...
+    b: list[str]
+    a: list[str]
+    a, b = data
+    return b.filtered(lambda x: make(x, a)).len()
 
 
 aoc_helper.lazy_test(day=19, year=2024, parse=parse_raw, solution=part_one)
@@ -45,7 +61,21 @@ aoc_helper.lazy_test(day=19, year=2024, parse=parse_raw, solution=part_one)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    ...
+    b: list[str]
+    a: list[str]
+    a, b = data
+
+    @cache
+    def make(s: str):
+        if not s:
+            return 1
+        tot = 0
+        for x in a:
+            if s.startswith(x):
+                tot += make(s[len(x) :])
+        return tot
+
+    return b.mapped(lambda x: make(x)).sum()
 
 
 aoc_helper.lazy_test(day=19, year=2024, parse=parse_raw, solution=part_two)
