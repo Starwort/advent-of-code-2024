4aa3260b9b8d1df166bc9a94a067a6aac6a2263d
starwort <tcphone93@gmail.com>
2024-12-23 05:09:16 +0000

    342/235

--- day_23.py
+++ day_23.py
@@ -25,7 +25,7 @@ raw = aoc_helper.fetch(23, 2024)
 
 
 def parse_raw(raw: str):
-    return ...
+    return list(raw.splitlines()).mapped(lambda i: list(i.split("-")))
 
 
 data = parse_raw(raw)
@@ -35,7 +35,21 @@ data = parse_raw(raw)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    ...
+    all_connections = {}
+    for computer in set(data.flat()):
+        connections = set()
+        for a, b in data:
+            if a == computer:
+                connections.add(b)
+            if b == computer:
+                connections.add(a)
+        all_connections[computer] = connections
+    groups = set()
+    for computer, others in all_connections.items():
+        for other in others:
+            for last in others & all_connections[other]:
+                groups.add(frozenset([computer, other, last]))
+    return iter(groups).filter(lambda i: any(j[0] == "t" for j in i)).count()
 
 
 aoc_helper.lazy_test(day=23, year=2024, parse=parse_raw, solution=part_one)
@@ -45,7 +59,25 @@ aoc_helper.lazy_test(day=23, year=2024, parse=parse_raw, solution=part_one)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    ...
+    all_connections = {}
+    for computer in set(data.flat()):
+        connections = set()
+        for a, b in data:
+            if a == computer:
+                connections.add(b)
+            if b == computer:
+                connections.add(a)
+        all_connections[computer] = connections
+    groups = set()
+    for computer, others in all_connections.items():
+        for other in others:
+            rest_of_eligible_group = others & all_connections[other]
+            group = frozenset([computer, other]) | rest_of_eligible_group
+            for i in rest_of_eligible_group:
+                if i in group:
+                    group &= {i} | all_connections[i]
+            groups.add(group)
+    return ",".join(sorted(iter(groups).max(len)))
 
 
 aoc_helper.lazy_test(day=23, year=2024, parse=parse_raw, solution=part_two)
